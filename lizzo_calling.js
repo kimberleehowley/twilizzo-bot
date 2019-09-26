a0efdc42eba3c4b41ffb0a8d050a434b71d36196
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-09-26 15:06:17 -0700

    First rev of code using the Twiml demo easter egg code

--- lizzo_calling.js
+++ lizzo_calling.js
@@ -1,3 +1,11 @@
 // Requiring environment variables
 require('dotenv').config()
 const client = require('twilio')(); 
+
+client.calls
+    .create({
+        url: 'http://demo.twilio.com/docs/voice.xml', 
+        to: process.env.TO_NUMBER, 
+        from: process.env.FROM_NUMBER
+    })
+    .then(call => console.log("Lizzo calling!"))
